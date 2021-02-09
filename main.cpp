c889c925c81e571eea9a3d2ba6a16adb374a723d
M1andy <M1andy@mail.dlut.edu.cn>
2021-02-10 00:03:41 +0800

    添加如何使用sort排序

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> arr {5,6,123,542,78};
+    for(auto s: arr){
+        cout << s << " ";
+    }
+    cout << endl;
+
+    sort(arr.begin(), arr.end());
+    for(auto s: arr){
+        cout << s << " ";
+    }
+    cout << endl;
+
     return 0;
 }
