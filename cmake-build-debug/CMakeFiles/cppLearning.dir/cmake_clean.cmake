305e21b7df4c3c742cdbc06567bb20be754ee618
M1andy <M1andy@mail.dlut.edu.cn>
2021-02-09 23:58:28 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/cppLearning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppLearning.dir/main.cpp.obj"
+  "cppLearning.exe"
+  "cppLearning.exe.manifest"
+  "cppLearning.pdb"
+  "libcppLearning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
